ab44f9586e9ca38aade8847feccb9f2b76757b5c
lunjesse <30811237+lunjesse@users.noreply.github.com>
2017-12-20 17:35:08 -0500

    Update Mario Bros. (Classic).lua
    removed unused code + placed comments elsewhere

--- Mario Bros. (Classic).lua
+++ Mario Bros. (Classic).lua
@@ -1,20 +1,6 @@
 memory.usememorydomain("Combined WRAM")
 
-function move(dest)
-
-end
-
-while true do
-local start_address = 0x03C140 
-local blocks = memory.readbyte(0x03C12E)	--number of blocks atm
-local block_state = 0
-local block_pos = 0
-local right_char = memory.readbyte(0x03C138)	--move with a/b
-local left_char = memory.readbyte(0x03C139)	--move with up/down
-	for i = 0, blocks do
-		block_state = memory.readbyte(start_address+(i*4))
-		if (block_state == 1) then	--it exists, and is not being carried to next section
-		--[[
+--[[
 6 cases:
 right char
 	2 (bottom)
@@ -24,7 +10,20 @@ left char
 	11 (bottom)
 	29 (middle)
 	47 (top)
-		]]--
+]]--
+
+while true do
+local start_address = 0x03C140 
+local blocks = memory.readbyte(0x03C12E)	--number of blocks atm
+local block_state = 0
+local block_pos = 0
+local right_char = memory.readbyte(0x03C138)	--move with a/b
+local left_char = memory.readbyte(0x03C139)	--move with up/down
+
+	for i = 0, blocks do
+		block_state = memory.readbyte(start_address+(i*4))
+		if (block_state == 1) then	--it exists, and is not being carried to next section
+
 			block_pos = memory.readbyte(start_address+(i*4)+1)
 			if (block_pos == 2) then
 				if (right_char > 0) then joypad.set({B = true}) end
@@ -39,6 +38,7 @@ left char
 			elseif (block_pos == 47) then
 				if (left_char < 2) then joypad.set({Up = true}) end
 			end
+			emu.frameadvance()
 		end
 	end
 	emu.frameadvance()

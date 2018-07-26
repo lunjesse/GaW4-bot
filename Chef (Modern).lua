c25f75b21b208f68c30db7a9881107ef70d445de
lunjesse <30811237+lunjesse@users.noreply.github.com>
2018-07-25 22:24:28 -0400

    Update Chef (Modern).lua
    1. Combined the travel functions, and named it "feed_yoshi"
    2. Cleaned up some "magic numbers" to make it more readable
    3. Added more comments

--- Chef (Modern).lua
+++ Chef (Modern).lua
@@ -1,224 +1,159 @@
 memory.usememorydomain("Combined WRAM")
-food_address = {[1] = 0x03C748, [2] = 0x03C75C, [3] = 0x03C770, [4] = 0x03C784}
-x_yoshi = 0x03C358
+local food_address = {[1] = 0x03C748, [2] = 0x03C75C, [3] = 0x03C770, [4] = 0x03C784}
+local food_state = {["Raw"]=0,["Cooked"]=1,["Burnt"]=2}
+--addresses
+local yoshi_x = 0x03C358
+local peach_x = 0x03C351	--where peach is
+local peach_dir = 0x03C352	--where is peach facing
+local yoshi_time_adr = 0x03C634
+local yoshi_food_count = 0x03C35B	--how much yoshi has ate in this form
 --[[
 Peach 03C351
 Peach direction 03C352
 0 is left, 1 is right
 If food is at col 0, peach should be at col 0 and 0 direction
-
 If food is at col 1, peach should be at col 0 and 1 direction
 If food is at col 1, peach should be at col 1 and 0 direction
-
 If food is at col 2, peach should be at col 1 and 1 direction
 If food is at col 2, peach should be at col 2 and 0 direction
-
 If food is at col 3, peach should be at col 2 and 1 direction
 If food is at col 3, peach should be at col 3 and 0 direction
-
 If food is at col 4, peach should be at col 3 and 1 direction
 Yoshi should be at whatever col food is if its the target
 ]]--
 
+--advance frame by "frames" amount.
+local function frame_advance(frames)
+	if frames ~= nil then 
+		repeat
+			emu.frameadvance()
+			frames = frames - 1
+		until frames == 0
+	else
+		emu.frameadvance()
+	end
+end
 
 
 local function move(direction)
 	if direction == "right" then
 		joypad.set({Right = true}) 
-		emu.frameadvance()
-		emu.frameadvance()
-		emu.frameadvance()
+		frame_advance(3)
 	elseif direction == "left" then
 		joypad.set({Left = true}) 
-		emu.frameadvance()
-		emu.frameadvance()
-		emu.frameadvance()
+		frame_advance(3)
 	end
 end
-local function travel(destination)
-	local peach = memory.readbyte(0x03C351)
-	local direction = memory.readbyte(0x03C352)
-	local left = {[0]=true,[2]=true,[8]=true,[12]=true}
-	local right = {[1]=true,[3]=true,[9]=true,[13]=true}
-	
-	if destination == 0 then	--food is far left side
-		if peach > 0 then
-			repeat
-				move("left")
-			until memory.readbyte(0x03C351) == 0	--otherwise "peach" never updates
-		elseif peach == 0 then
-			if left[direction] == nil then
-				joypad.set({A = true}) 
-				emu.frameadvance()
-			end
-		end
-	
-	elseif destination == 4 then	--food is far right side
-		if peach < 3 then
-			repeat
-				move("right")
-			until memory.readbyte(0x03C351) == 3	--otherwise "peach" never updates
-		elseif peach == 3 then
-			if right[direction] == nil then
-				joypad.set({A = true}) 
-				emu.frameadvance()
-			end
-		end
-		
-	elseif destination == 1 then	--food is 1 step to the right
-		if peach == 0 then
-			if right[direction] == nil then
-				joypad.set({A = true}) 
-				emu.frameadvance()
-			end
-		end
-		if peach == 1 then
-			if left[direction] == nil then
-				joypad.set({A = true}) 
-				emu.frameadvance()
-			end
-		end
-		if peach > 1 then
-			repeat
-				move("left")
-			until memory.readbyte(0x03C351) == 1	--otherwise "peach" never updates
-		end
-	
-	elseif destination == 2 then	--food is 2 steps to the right
-		if peach > 2 then
-			repeat
-				move("left")
-			until memory.readbyte(0x03C351) == 2	--otherwise "peach" never updates
-		end
-		if peach < 1 then
-			repeat
-				move("right")
-			until memory.readbyte(0x03C351) == 1	--otherwise "peach" never updates
-		end
-		
-		if peach == 1 then
-			if right[direction] == nil then
-				joypad.set({A = true}) 
-				emu.frameadvance()
-			end
-		end
-		if peach == 2 then
-			if left[direction] == nil then
-				joypad.set({A = true}) 
-				emu.frameadvance()
-			end
-		end
-		
-	elseif destination == 3 then	--food is 3 steps to the right
-		if peach < 2 then
-			repeat
-				move("right")
-			until memory.readbyte(0x03C351) == 2	--otherwise "peach" never updates
-		end
-		if peach == 2 then
-			if right[direction] == nil then
-				joypad.set({A = true}) 
-				emu.frameadvance()
-			end
-		end
-		if peach == 3 then
-			if left[direction] == nil then
-				joypad.set({A = true}) 
-				emu.frameadvance()
-			end
-		end	
-	end		--this ends destination if
-end
 
-function travel2(destination)
+function feed_yoshi(destination,action)
+--action is feed or cook
 	--Yoshi is always behind peach, so ignore him
-	local peach = memory.readbyte(0x03C351)
-	local direction = memory.readbyte(0x03C352)
+	local peach = memory.readbyte(peach_x)
+	local direction = memory.readbyte(peach_dir)
 	local left = {[0]=true,[2]=true,[8]=true,[12]=true}
 	local right = {[1]=true,[3]=true,[9]=true,[13]=true}
+	local turned_correctly
+	--array for readibility purposes
+	local dest_array = {["far left"]=0,["left"]=1,["middle"]=2,["right"]=3,["far right"]=4}
+	local peach_array = {["far left"]=0,["left"]=1,["right"]=2,["far right"]=3}
 	
-	if destination == 0 then	--food is far left side
-		if peach > 0 then
+	if destination == dest_array["far left"] then	--food is far left side
+		if peach > peach_array["far left"] then
 			repeat
 				move("left")
-			until memory.readbyte(0x03C351) == 0	--otherwise "peach" never updates
-		elseif peach == 0 then
-			if right[direction] == nil then
-				joypad.set({A = true}) 
-				emu.frameadvance()
-			end
-		end
-	
-	elseif destination == 4 then	--food is far right side
-		if peach < 3 then
-			repeat
-				move("right")
-			until memory.readbyte(0x03C351) == 3	--otherwise "peach" never updates
-		elseif peach == 3 then
-			if left[direction] == nil then
-				joypad.set({A = true}) 
-				emu.frameadvance()
-			end
-		end
-		
-	elseif destination == 1 then	--food is 1 step to the right
-		if peach == 0 then
-			if left[direction] == nil then
-				joypad.set({A = true}) 
-				emu.frameadvance()
-			end
-		end
-		if peach == 1 then
-			if right[direction] == nil then
-				joypad.set({A = true}) 
-				emu.frameadvance()
-			end
-		end
-		if peach > 1 then
-			repeat
-				move("left")
-			until memory.readbyte(0x03C351) == 1	--otherwise "peach" never updates
-		end
-		
-	elseif destination == 2 then	--food is 2 steps to the right
-		if peach > 2 then
-			repeat
-				move("left")
-			until memory.readbyte(0x03C351) == 2	--otherwise "peach" never updates
-		end
-		if peach < 1 then
-			repeat
-				move("right")
-			until memory.readbyte(0x03C351) == 1	--otherwise "peach" never updates
-		end
-		
-		if peach == 1 then
-			if left[direction] == nil then
-				joypad.set({A = true}) 
-				emu.frameadvance()
-			end
-		end
-		if peach == 2 then
-			if right[direction] == nil then
+				peach = memory.readbyte(peach_x)	--updates peach address; otherwise never does
+			until peach == peach_array["far left"]
+		elseif peach == peach_array["far left"] then	--peach is at the right spot
+		--feed food, or cook it. [direction]== nil means Peach isn't turned correctly
+			turned_correctly = (action == "feed" and right[direction] == nil) or (action == "cook" and left[direction] == nil)
+			if turned_correctly then
 				joypad.set({A = true}) 
 				emu.frameadvance()
 			end
 		end
 	
-	elseif destination == 3 then	--food is 3 steps to the right
-		if peach < 2 then
+	elseif destination == dest_array["far right"] then	--food is far right side
+		if peach < peach_array["far right"] then
 			repeat
 				move("right")
-			until memory.readbyte(0x03C351) == 2	--otherwise "peach" never updates
-		end
-		if peach == 2 then
-			if left[direction] == nil then
+				peach = memory.readbyte(peach_x)	--updates peach address; otherwise never does
+			until peach == peach_array["far right"]	--otherwise "peach" never updates
+		elseif peach == peach_array["far right"] then
+			turned_correctly = (action == "feed" and left[direction] == nil) or (action == "cook" and right[direction] == nil)
+			if turned_correctly then
 				joypad.set({A = true}) 
 				emu.frameadvance()
 			end
 		end
-		if peach == 3 then
-			if right[direction] == nil then
+		
+	elseif destination == dest_array["left"] then	--food is 1 step to the right
+		if peach == peach_array["far left"] then
+			turned_correctly = (action == "feed" and left[direction] == nil) or (action == "cook" and right[direction] == nil)
+			if turned_correctly then
+				joypad.set({A = true}) 
+				emu.frameadvance()
+			end
+		end
+		if peach == peach_array["left"] then
+			turned_correctly = (action == "feed" and right[direction] == nil) or (action == "cook" and left[direction] == nil)
+			if turned_correctly then
+				joypad.set({A = true}) 
+				emu.frameadvance()
+			end
+		end
+		if peach > peach_array["left"] then
+			repeat
+				move("left")
+				peach = memory.readbyte(peach_x)	--updates peach address; otherwise never does
+			until peach == peach_array["left"]
+		end
+		
+	elseif destination == dest_array["middle"] then	--food is 2 steps to the right
+		if peach > peach_array["right"] then
+			repeat
+				move("left")
+				peach = memory.readbyte(peach_x)	--updates peach address; otherwise never does
+			until peach == peach_array["right"]
+		end
+		if peach < peach_array["left"] then
+			repeat
+				move("right")
+				peach = memory.readbyte(peach_x)	--updates peach address; otherwise never does
+			until peach == peach_array["left"]
+		end
+		
+		if peach == peach_array["left"] then
+			turned_correctly = (action == "feed" and left[direction] == nil) or (action == "cook" and right[direction] == nil)
+			if turned_correctly then
+				joypad.set({A = true}) 
+				emu.frameadvance()
+			end
+		end
+		if peach == peach_array["right"] then
+			turned_correctly = (action == "feed" and right[direction] == nil) or (action == "cook" and left[direction] == nil)
+			if turned_correctly then
+				joypad.set({A = true}) 
+				emu.frameadvance()
+			end
+		end
+	
+	elseif destination == dest_array["right"] then	--food is 3 steps to the right
+		if peach < peach_array["right"] then
+			repeat
+				move("right")
+				peach = memory.readbyte(peach_x)	--updates peach address; otherwise never does
+			until peach == peach_array["right"]
+		end
+		if peach == peach_array["right"] then
+			turned_correctly = (action == "feed" and left[direction] == nil) or (action == "cook" and right[direction] == nil)
+			if turned_correctly then
+				joypad.set({A = true}) 
+				emu.frameadvance()
+			end
+		end
+		if peach == peach_array["far right"] then
+			turned_correctly = (action == "feed" and right[direction] == nil) or (action == "cook" and left[direction] == nil)
+			if turned_correctly then
 				joypad.set({A = true}) 
 				emu.frameadvance()
 			end
@@ -238,12 +173,16 @@ local function GetStats(addr,o)
     --... And so on
     return o
 end
+
+
 while true do
 	local food = {}
+	local direction = memory.readbyte(peach_dir)
 	local nearest_y = 0
 	local nearest_i = 0
 	local nearest_x = 0
-	local yoshi_time = memory.readbyte(0x3C634)
+	local yoshi_time = memory.readbyte(yoshi_time_adr)
+	local action = "Nothing"
 	for i = 1, 4 do
 		food[i] = GetStats(food_address[i])
 		if food[i].exists > 0 then
@@ -256,19 +195,23 @@ while true do
 			end
 		end
 	end
-	if nearest_y > 0 and food[nearest_i].state ~= 1 then	--it's not raw nor burnt
-		travel(nearest_x)
-	elseif nearest_y > 0 and food[nearest_i].state == 1 then
-		if (memory.readbyte(0x03C634) == 0 or memory.readbyte(0x03C634) == 24) then
-			travel2(nearest_x)
+	if nearest_y > 0 and food[nearest_i].state ~= food_state["Cooked"] then	--it's not raw nor burnt
+		action = "cook"
+		feed_yoshi(nearest_x,action)
+	elseif nearest_y > 0 and food[nearest_i].state == food_state["Cooked"] then
+		if (yoshi_time == 0 or yoshi_time == 24) then
+			--yoshi is available to be fed; need to find a better way to determine this
+			action = "feed"
+			feed_yoshi(nearest_x,action)
 		else
-		travel(nearest_x)
+			--food is cooked, but yoshi is "busy", so don't atempt to feed
+			action = "cook"
+			feed_yoshi(nearest_x,action)
 		end
 	end
-	
 	gui.drawText(0,85,nearest_i..": Time:"..nearest_y.." X:"..nearest_x)
-	gui.drawText(0,100,"Yoshi:"..memory.readbyte(x_yoshi).."Food:"..memory.readbyte(0x03C35B))
-	gui.drawText(0,145,"Peach:"..memory.readbyte(0x03C351))
+	gui.drawText(0,100,"Yoshi:"..memory.readbyte(yoshi_x).."Food:"..memory.readbyte(yoshi_food_count))
+	gui.drawText(0,145,"Peach:"..memory.readbyte(peach_x).."("..direction..")")
+	gui.drawText(0,130,action)
 	emu.frameadvance()
 end
-
